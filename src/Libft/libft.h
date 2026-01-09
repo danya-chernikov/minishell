774e54d07ccfb9401d12eb5b9f7f7f640b6808cc
danya-chernikov <dchernik@proton.me>
2026-01-09 23:38:29 +0100

    The get_next_line() function was added to our Libft. We now have a working, though not entirely correct, method for identifying the UID and GID of the user who invoked our shell (see the `get_username.c` file). Again, we are not permitted to use standard system calls such as getuid(), getgid(), or getpwuid()

--- src/Libft/libft.h
+++ src/Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: dchernik <dchernik@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 18:40:28 by dchernik          #+#    #+#             */
-/*   Updated: 2026/01/06 12:52:43 by dchernik         ###   ########.fr       */
+/*   Updated: 2026/01/09 22:41:32 by dchernik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,39 @@
 # define LIBFT_H
 
 # include <stddef.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# define EXIT			0
+# define READ			1
+# define ALLOC			2
+# define END			3
+# define AGAIN			4
+
+/* FD		  - a file descriptor;
+ * RLEN		  - bytes read from `fd` (FD);
+ * I		  - an auxiliary coutner variable;
+ * RES		  - result of a function execution;
+ * PRIV_MEM_S - previously allocated memory size. */
+# define FD				0
+# define BUF_POS		1
+# define LINE_POS		2
+# define RLEN			3
+# define LINE_LEN		4
+# define I				5
+# define RES			6
+# define PRIV_MEM_S		7
+
+/* NORM  - normal execution. Means to do nothing;
+ * RET   - indicate that the `line` must be returned;
+ * BREAK - break from the main loop must be performed;
+ * CONT  - continue must be applied on the main loop. */
+# define NORM			0
+# define RET			1
+# define BREAK			2
+# define CONT			3
 
 typedef struct s_list
 {
@@ -58,6 +91,7 @@ void		ft_striteri(char *s, void (*f)(unsigned int, char *));
 char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char		*ft_substr(char const *s, unsigned int start, size_t len);
 
+/* Singly-linked list */
 t_list		*ft_lstnew(void *content);
 void		ft_lstadd_front(t_list **lst, t_list *new);
 int			ft_lstsize(t_list *lst);
@@ -67,6 +101,19 @@ void		ft_lstdelone(t_list *lst, void (*del)(void *));
 void		ft_lstclear(t_list **lst, void (*del)(void *));
 void		ft_lstiter(t_list *lst, void (*f)(void *));
 t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
-void		ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)());
+void		ft_list_remove_if(t_list **beg_list, void *data_ref, int (*cmp)());
+
+/* get_next_line.c */
+char	*get_next_line(int fd);
+int		loop_alg(char *buf, char **line, long long *v, int *flags);
+int		get_chunk(char *buf, char **line, long long *v, int *flags);
+void	process_new_line(char *buf, char **line, long long *v, int *flags);
+int		process_end_chunk(char **line, long long *v, int *flags);
+
+/* get_next_line_utils.c */
+int		init(char *buf, char **line, long long *v, int *flags);
+int		alloc_mem(char **line, long long *v, int *flags);
+void	check_reaching_end(long long *v, int *flags);
+void	clear_func_state(char **line, long long *v, int *flags);
 
 #endif
---
+++ tests/get_username.c
@@ -0,0 +1,74 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "libft.h"
+
+#define PASSWD_PATH	"/etc/passwd"
+
+void split_free(char **s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		free(s[i++]);
+	free(s);
+}
+
+int	main()
+{
+	char		*term_name;
+	struct stat	sb;
+	ssize_t		ret;
+	uid_t		my_uid;
+	gid_t		my_gid;
+	int			fd;
+	char		*line;
+	char		**ptokens;
+
+	term_name = ttyname(STDIN_FILENO);
+	if (!term_name)
+	{
+		perror("ttyname");
+		return 1;
+	}
+	ret = stat(term_name, &sb);
+	if (ret)
+	{
+		perror("stat");
+		return 1;
+	}
+	my_uid = sb.st_uid;
+	my_gid = sb.st_gid;
+	printf("My UID is %d\n", my_uid);
+	printf("My GID is %d\n", my_gid);
+
+	fd = open(PASSWD_PATH, O_RDONLY);
+	if (fd == -1)
+	{
+		perror("open");
+		return 1;
+	}
+	do
+	{
+		line = get_next_line(fd);
+		if (line)
+		{
+			ptokens	= ft_split(line, ':');
+			if (ft_atoi(ptokens[2]) == my_uid)
+			{
+				printf("My username is %s\n", ptokens[0]);
+				printf("My home directory is %s\n", ptokens[5]);
+			}
+			split_free(ptokens);
+		}
+		free(line);
+	} while (line);
+	close(fd);
+	return 0;
+}
