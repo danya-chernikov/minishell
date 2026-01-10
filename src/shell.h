aa19d1a681989df4f7bb4d8a239ad5bca04d634f
danya-chernikov <dchernik@proton.me>
2026-01-10 20:38:42 +0100

    Some structures were added to implement initialization state later

--- src/shell.h
+++ src/shell.h
@@ -2,16 +2,30 @@
 # define SHELL_H
 
 # include "init.h"
+# include "vector.h"
 
 # define MINISHELL_VERSION	"1.0-release"
 
 /* The default path is used when
  * it isn't inherited from the
  * parent or found in any configs */
-# define DEF_PATH "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+# define DEF_PATH			"/usr/local/sbin:"
+	"/usr/local/bin:"
+	"/usr/sbin:"
+	"/usr/bin:"
+	"/sbin:/bin"
+/* When unsetting both HISTSIZE and HISTFILESIZE,
+ * they revert to their default values and history
+ * continues to work. However, when HISTFILE is
+ * unset, writing history to the file in the current
+ * shell stops working. In a child shell, the value
+ * of HISTFILE is reset to the default, and history
+ * is written without any problems */
+# define DEF_HISTSIZE		500
+# define DEF_HISTFILESIZE	500
 
 /* If executed with the `--bash-compliant`
- * option minishell will resd bash configs */
+ * option minishell will read bash configs */
 
 /* LOGIN SHELL*/
 
@@ -71,5 +85,34 @@ typedef enum e_shell_mode
 	INT_LOG,
 	INT_NONLOG
 }	t_shell_mode;
+
+/* Type of the history command.
+ * Determines whether this
+ * command was read from the
+ * history file (in which case
+ * it should not be written to
+ * the history file again) or
+ * entered during the current
+ * session (in which case it
+ * should be written to the
+ * history file, since it is
+ * a new command) */
+typedef enum e_hist_type
+{
+	FROM_FILE,
+	FROM_SESSION
+}	t_hist_type;
+
+typedef struct e_hist_cmd
+{
+	t_vector	cmd;
+	t_hist_type	type;
+};
+
+/* Special shell parameters */
+struct e_shell_params
+{
+
+} t_shell_params;
 
 #endif
