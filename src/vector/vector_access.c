7d94e07a6e43857d35eecdc91dcda61cc3f9cc46
danya-chernikov <dchernik@proton.me>
2026-02-25 20:55:07 +0100

    42 headers were added

--- src/vector/vector_access.c
+++ src/vector/vector_access.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector_access.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dchernik <dchernik@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/25 20:47:51 by dchernik          #+#    #+#             */
+/*   Updated: 2026/02/25 20:47:52 by dchernik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "vector.h"
 
 void	*vector_at(const t_vector *v, size_t ind)
@@ -10,13 +22,13 @@ void	*vector_mat(const t_vector *v, size_t ri, size_t ci)
 	return (v->data + (((v->col_n * ri) + ci)) * v->esize);
 }
 
-// Access the first element
+/* Access the first element */
 void	*vector_front(const t_vector *v)
 {
 	return (v->front);
 }
 
-// Access the last element
+/* Access the last element */
 void	*vector_back(const t_vector *v)
 {
 	return (v->back);
