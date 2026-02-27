cff3d13abcd80b7b0df0e057fa567318f2e2215c
danya-chernikov <dchernik@proton.me>
2026-02-27 19:01:42 +0100

    The problem with parsing redirections was solved. Before, when we ran: $ >infile without a space, our shell interpreted '>infile' as an executable name

--- src/exp_preparation.c
+++ src/exp_preparation.c
@@ -8,64 +8,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Divide operand's string 'name' on tokens by spaces
- * (which are located outside any quote intervals of
- * course). Each operand contains at least one token */
-/*int	exp_divide_op_str_on_tokens(t_operand *op)
-{
-	size_t		op_i;
-	size_t		ti;
-	size_t		opstr_len;
-	char		*op_str;
-	char		*tcnt; // Operand's token content
-
-	op_str = op->name;
-	opstr_len = ft_strlen(op_str);
-
-	tcnt = (char *)malloc(MAX_OP_TOKEN_LEN * sizeof(char));
-	if (!tcnt)
-	{
-		perror("malloc");
-		return (COMMON_SYS_ERR);
-	}
-	ti = 0;
-	op_i = 0;
-	while (op_i < opstr_len)
-	{
-		if (op_i >= MAX_OP_TOKEN_LEN - 1) // Overflow check
-		{
-			print_shell_error(NULL, MAX_OP_TOK_LEN_ERR_MSG);
-			return (COMMON_FAILURE);
-		}
-		if (op_str[op_i] == ' ' && !is_inside_op_quotes(op, op_i))
-		{
-			if (op->token_cnt > MAX_OP_TOKENS_NUM - 1) // Overflow check
-			{
-				print_shell_error(NULL, MAX_OP_TOK_NUM_ERR_MSG);
-				return (COMMON_FAILURE);
-			}
-			tcnt[ti] = '\0';
-			ti = 0;
-			op->tokens[op->token_cnt].cnt = tcnt;
-			++op->token_cnt;
-			tcnt = (char *)malloc(MAX_OP_TOKEN_LEN * sizeof(char));
-			if (!tcnt)
-			{
-				perror("malloc");
-				return (COMMON_SYS_ERR);
-			}
-			skip_spaces(op_str, &op_i);
-			continue ;
-		}
-		tcnt[ti] = op_str[op_i];
-		++op_i;
-		++ti;
-	}
-	tcnt[ti] = '\0';
-	op->tokens[op->token_cnt].cnt = tcnt;
-	++op->token_cnt;
-	return (COMMON_SUCCESS);
-}*/
+static bool	is_redir_split(t_operand *op, size_t i);
+static int	push_redir_token(t_operand *op, size_t *i, size_t len);
+static int	push_word_token(t_operand *op, size_t *i, size_t len);
+static int	create_token(t_operand *op, size_t start, size_t tok_len);
 
 /* Divide operand->name into op->tokens[].cnt by
  * spaces outside quotes. Allocates each token
@@ -76,68 +22,25 @@ int	exp_divide_op_str_on_tokens(t_operand *op)
 {
 	size_t	i;
 	size_t	len;
-	char	*op_str;
+	int		res;
 
 	if (!op || !op->name || !op->tokens)
 		return (COMMON_FAILURE);
-
 	op->token_cnt = 0;
-
-	op_str = op->name;
-	len = ft_strlen(op_str);
 	i = 0;
-
+	len = ft_strlen(op->name);
 	while (i < len)
 	{
-		size_t	start;
-		size_t	tok_len;
-		char	*tok;
-
-		/* Skip spaces */
-		while (i < len && op_str[i] == ' ')
+		while (i < len && op->name[i] == ' ' && !is_inside_op_quotes(op, i))
 			++i;
 		if (i >= len)
-			break;
-
-		start = i;
-
-		/* Read until next unquoted space */
-		while (i < len)
-		{
-			if (op_str[i] == ' ' && !is_inside_op_quotes(op, i))
-				break;
-			++i;
-		}
-
-		tok_len = i - start;
-
-		if (tok_len >= MAX_OP_TOKEN_LEN)
-		{
-			print_shell_error(NULL, MAX_OP_TOK_LEN_ERR_MSG);
-			exp_free_op_tokens(op);
-			return (COMMON_FAILURE);
-		}
-		if (op->token_cnt >= MAX_OP_TOKENS_NUM)
-		{
-			print_shell_error(NULL, MAX_OP_TOK_NUM_ERR_MSG);
-			exp_free_op_tokens(op);
-			return (COMMON_FAILURE);
-		}
-
-		tok = (char *)malloc((tok_len + 1) * sizeof (char));
-		if (!tok)
-		{
-			perror("malloc");
-			exp_free_op_tokens(op);
-			return (COMMON_SYS_ERR);
-		}
-		ft_memcpy(tok, op_str + start, tok_len);
-		tok[tok_len] = '\0';
-
-		op->tokens[op->token_cnt].cnt = tok;
-		op->tokens[op->token_cnt].qpair_cnt = 0;
-		op->tokens[op->token_cnt].redir_ind = -1;
-		++op->token_cnt;
+			break ;
+		if (is_redir_split(op, i))
+			res = push_redir_token(op, &i, len);
+		else
+			res = push_word_token(op, &i, len);
+		if (res != COMMON_SUCCESS)
+			return (exp_free_op_tokens(op), res);
 	}
 	return (COMMON_SUCCESS);
 }
@@ -160,4 +63,77 @@ int exp_update_op_tokens_quote_intervals(t_operand *op)
 		++ti;
 	}
 	return (fret);
+}
+
+static bool	is_redir_split(t_operand *op, size_t i)
+{
+	if (is_inside_op_quotes(op, i))
+		return (false);
+	if (op->name[i] == '>' || op->name[i] == '<')
+		return (true);
+	return (false);
+}
+
+static int	push_redir_token(t_operand *op, size_t *i, size_t len)
+{
+	size_t	tok_len;
+
+	tok_len = 1;
+	if (*i + 1 < len
+		&& op->name[*i + 1] == op->name[*i]
+		&& !is_inside_op_quotes(op, *i + 1))
+	{
+		tok_len = 2;
+	}
+	if (create_token(op, *i, tok_len) != COMMON_SUCCESS)
+		return (COMMON_FAILURE);
+	*i += tok_len;
+	return (COMMON_SUCCESS);
+}
+
+static int	push_word_token(t_operand *op, size_t *i, size_t len)
+{
+	size_t	start;
+
+	start = *i;
+	while (*i < len)
+	{
+		if (!is_inside_op_quotes(op, *i) && op->name[*i] == ' ')
+			break ;
+		if (is_redir_split(op, *i))
+			break ;
+		++(*i);
+	}
+	if (create_token(op, start, *i - start) != COMMON_SUCCESS)
+		return (COMMON_FAILURE);
+	return (COMMON_SUCCESS);
+}
+
+static int	create_token(t_operand *op, size_t start, size_t tok_len)
+{
+	char	*tok;
+
+	if (op->token_cnt >= MAX_OP_TOKENS_NUM)
+	{
+		print_shell_error(NULL, MAX_OP_TOK_NUM_ERR_MSG);
+		return (COMMON_FAILURE);
+	}
+	if (tok_len >= MAX_OP_TOKEN_LEN)
+	{
+		print_shell_error(NULL, MAX_OP_TOK_LEN_ERR_MSG);
+		return (COMMON_FAILURE);
+	}
+	tok = (char *)malloc((tok_len + 1) * sizeof(char));
+	if (!tok)
+	{
+		perror("malloc");
+		return (COMMON_SYS_ERR);
+	}
+	ft_memcpy(tok, op->name + start, tok_len);
+	tok[tok_len] = '\0';
+	op->tokens[op->token_cnt].cnt = tok;
+	op->tokens[op->token_cnt].qpair_cnt = 0;
+	op->tokens[op->token_cnt].redir_ind = -1;
+	++op->token_cnt;
+	return (COMMON_SUCCESS);
 }
